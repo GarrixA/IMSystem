1d3b163f04a2f04ce37ac4abc71f2a21550af73c
GarrixA <aphrogarrix10@gmail.com>
2025-02-17 07:57:15 +0200

    fix(role): fix admin and manager navigations

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased bg-white">
+      <body className="antialiased bg-[#CCE2F3]">
         {children}
 
         <ToastContainer />
